b407ff1eafbed45fa58f319320dfab35bdb40a6b
Rebeccaqaz <2176871914@qq.com>
2019-06-06 23:56:06 +0800

    first

---
+++ day02/xiaomitop.js
@@ -0,0 +1,61 @@
+function slider1(){
+	document.getElementById("move_1").style.display="block"
+	document.getElementById("move_2").style.display="none"
+	document.getElementById("move_3").style.display="none"
+	document.getElementById("move_4").style.display="none"
+	document.getElementById("move_5").style.display="none"
+}
+function slider2(){
+	document.getElementById("move_2").style.display="block"
+	document.getElementById("move_1").style.display="none"
+	document.getElementById("move_3").style.display="none"
+	document.getElementById("move_4").style.display="none"
+	document.getElementById("move_5").style.display="none"
+}
+function slider3(){
+	document.getElementById("move_3").style.display="block"
+	document.getElementById("move_2").style.display="none"
+	document.getElementById("move_1").style.display="none"
+	document.getElementById("move_4").style.display="none"
+	document.getElementById("move_5").style.display="none"
+}
+function slider4(){
+	document.getElementById("move_4").style.display="block"
+	document.getElementById("move_2").style.display="none"
+	document.getElementById("move_3").style.display="none"
+	document.getElementById("move_1").style.display="none"
+	document.getElementById("move_5").style.display="none"
+}
+function slider5(){
+	document.getElementById("move_5").style.display="block"
+	document.getElementById("move_2").style.display="none"
+	document.getElementById("move_3").style.display="none"
+	document.getElementById("move_4").style.display="none"
+	document.getElementById("move_1").style.display="none"
+}
+
+
+function remove1(){
+	document.getElementById("body_1").style.display="block"
+	document.getElementById("body_2").style.display="none"
+	document.getElementById("body_3").style.display="none"
+	document.getElementById("body_4").style.display="none"
+}
+function remove2(){
+	document.getElementById("body_2").style.display="block"
+	document.getElementById("body_1").style.display="none"
+	document.getElementById("body_3").style.display="none"
+	document.getElementById("body_4").style.display="none"
+}
+function remove3(){
+	document.getElementById("body_3").style.display="block"
+	document.getElementById("body_2").style.display="none"
+	document.getElementById("body_1").style.display="none"
+	document.getElementById("body_4").style.display="none"
+}
+function remove4(){
+	document.getElementById("body_4").style.display="block"
+	document.getElementById("body_2").style.display="none"
+	document.getElementById("body_3").style.display="none"
+	document.getElementById("body_1").style.display="none"
+}
